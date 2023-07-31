14299b47ef32c71628ace03f2f25f165d6204871
AdrianIon218 <ionadrian218@yahoo.com>
2023-07-31 23:58:22 +0300

    add no page

--- Desktop/cvpage/src/App.js
+++ Desktop/cvpage/src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/Layout/Layout'
 import Contact from './components/Contact/Contact'
 import CV from './components/CV/CV'
 import Projects from './components/Projects/Projects'
+import NoPage from './components/NoPage/NoPage'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='CV' element={<CV />} />
         <Route path='Projects' element={<Projects />} />
         <Route path='Contact' element={<Contact />} />
+        <Route path='*' element={<NoPage />} />
       </Routes>
     </Layout>
   );
