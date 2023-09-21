f637650276f82644342ec9d8a382735a308ad205
AdrianIon218 <ionadrian218@yahoo.com>
2023-09-21 23:42:08 +0300

    made some adjustments, added the loading spinner

--- Desktop/cvpage/src/App.js
+++ Desktop/cvpage/src/App.js
@@ -1,21 +1,25 @@
-import {Route, Routes} from 'react-router-dom'
-import React from 'react'
-import Layout from './components/Layout/Layout'
-import Contact from './components/Contact/Contact'
-import CV from './components/CV/CV'
-import Projects from './components/Projects/Projects'
-import NoPage from './components/NoPage/NoPage'
+import {Route, Routes} from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import Layout from './components/Layout/Layout';
+import LoadingSpinner from './components/Layout/LoadingSpinner';
+
+const CV = lazy(() => import("./components/CV/CV"));
+const Contact = lazy(() => import("./components/Contact/Contact"));
+const Projects = lazy(() => import("./components/Projects/Projects"));
+const NoPage = lazy(() => import("./components/NoPage/NoPage"));
 
 function App() {
   return (
     <Layout>
-      <Routes>        
-        <Route path="/" element={<CV />} />
-        <Route path='CV' element={<CV />} />
-        <Route path='Projects' element={<Projects />} />
-        <Route path='Contact' element={<Contact />} />
-        <Route path='*' element={<NoPage />} />
-      </Routes>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>        
+          <Route path="/" element={<CV />} />
+          <Route path='CV' element={<CV />} />
+          <Route path='Projects' element={<Projects />} />
+          <Route path='Contact' element={<Contact />} />
+          <Route path='*' element={<NoPage />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
