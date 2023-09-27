e31be115f1d89137392c4b031b0800efac8e80de
AdrianIon218 <ionadrian218@yahoo.com>
2023-09-28 00:01:30 +0300

    worked on presentation of projects

--- Desktop/cvpage/src/components/Projects/ProjectPanel.js
+++ Desktop/cvpage/src/components/Projects/ProjectPanel.js
@@ -4,6 +4,7 @@ import classes from './Projects.module.css';
 function ProjectPanel(props) {
   const panelRef= useRef();
   const [presentationElements, setPresentation] = useState([]);
+  const isThePortfolioPage = props.title !== "Portfolio";
 
   const closePresentation = (e)=>{
     panelRef.current.classList.add(classes.close_panel);
@@ -22,12 +23,15 @@ function ProjectPanel(props) {
 
   return (
     <div className={classes.backdrop} onClick={closePresentation}>
-        <div className={classes.project_presentation} ref={panelRef} onClick={(e => e.stopPropagation())}>
+      <div className={classes.project_presenation__container} ref={panelRef}>
+        {!isThePortfolioPage && <div className={classes.current_page_indicator}> The current page</div>}
+        <div className={classes.project_presentation} onClick={(e => e.stopPropagation())}>
            <h2>{props.title}</h2>
-           <button onClick={openCardLink} className={classes.btn_link}>Visit the website</button>
+           {isThePortfolioPage && <button onClick={openCardLink} className={classes.btn_link}>Visit the website</button>}
            {presentationElements}
         </div>
-        <button className={classes.exit_btn} onClick={closePresentation}>&times;</button>
+        <button className={classes.project_presenation__exit} onClick={closePresentation}>&times;</button>
+      </div>
     </div>
   )
 }
