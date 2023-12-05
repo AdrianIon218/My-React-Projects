1da481b55704c79abff359cea9e7a7e65b10f6be
AdrianIon218 <ionadrian218@yahoo.com>
2023-12-05 21:15:47 +0200

    minor changes

--- Desktop/cvpage/src/components/CV/Education.js
+++ Desktop/cvpage/src/components/CV/Education.js
@@ -3,14 +3,15 @@ import classes from "./CV.module.css";
 export default function Education(props) {
   return (
     <div className={classes.divColumn}>
-      <h3 className={classes.elementsAligned}>
+      <h3 className={`${classes.elementsAligned} ${classes["no-margin-y"]}`}>
         <i className="fas fa-school" />
         {props.universityName}
       </h3>
-      <ul>
+      <ul className={classes["no-margin-top"]}>
         <li>{props.facultyName}</li>
         <li>Degree : {props.degree}</li>
         <li>Field of Study : {props.fieldOfStudy}</li>
+        <li>Period : {props.period}</li>
       </ul>
     </div>
   );
--- Desktop/cvpage/src/components/CV/nationalityFlags.js
+++ Desktop/cvpage/src/components/CV/nationalityFlags.js
@@ -2,11 +2,7 @@ const NationalFlags = [
   {
     Nationality: "Romanian",
     FlagName: "romanianFlag.png",
-  },
-  {
-    Nationality: "Austrian",
-    FlagName: "austrianFlag.png",
-  },
+  }
 ];
 
 export default NationalFlags;
